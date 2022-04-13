28da6120a7897fd313d097e90d9d8e4209837017
OlegBovkush <obovkush@gmail.com>
2022-04-14 03:06:16 +1000

    Правильная middleware

--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -1,9 +1,8 @@
 const isAuthorized = (req, res, next) => {
-  if (req.session) {
-    res.locals.user = req.session?.user;
-    res.locals.isAuthorized = req.session?.isAuthorized;
-    next();
-  }
+  res.locals.user = req.session?.user;
+  res.locals.isAuthorized = req.session?.isAuthorized;
+  console.log('requser', req.session.user);
+  next();
 };
 
 module.exports = isAuthorized;
