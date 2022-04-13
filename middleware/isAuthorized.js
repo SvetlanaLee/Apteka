3e219bc5fe72508103ac8c2c765d2cc05c7cd28d
OlegBovkush <obovkush@gmail.com>
2022-04-14 03:10:27 +1000

    isAuthorised without console

--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -1,7 +1,6 @@
 const isAuthorized = (req, res, next) => {
   res.locals.user = req.session?.user;
   res.locals.isAuthorized = req.session?.isAuthorized;
-  console.log('requser', req.session.user);
   next();
 };
 
