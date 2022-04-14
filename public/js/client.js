ec5420805f2855fec1e5facd64ee04fa8201d677
Svetlana <lirica1109@gmail.com>
2022-04-14 23:33:20 +0300

    basket,order

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,21 @@
+// async function getPay(event) {
+//   console.log('hi');
+//   event.preventDefault();
+//   const data = {
+//     adress: event.target.adress.value,
+//     tel: event.target.tel.value,
+//     total: event.target.dataset.summa,
+//   };
+//   console.log(data);
+//   await fetch('/order/status', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   });
+// }
+
 async function addToBasket(event) {
   event.preventDefault();
   if (event.target.classList.contains('jsBtn')) {
@@ -17,4 +35,31 @@ async function addToBasket(event) {
   }
 }
 
+async function getOrder(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('orderBtn')) {
+    console.log(event.target.dataset.total);
+    const total = {
+      total: event.target.dataset.total,
+    };
+
+    const response = await fetch('/addToOrder', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(total),
+    });
+
+    window.location.href = '/order';
+    // const resHTML = await response.text();
+    // console.log(resHTML);
+    // document.querySelector('.js-order').innerHTML = resHTML;
+  }
+}
+
+// document.payOrder?.addEventListener('click', getPay);
+
 document.querySelector('.allCards')?.addEventListener('click', addToBasket);
+
+document.querySelector('.js-order')?.addEventListener('click', getOrder);
--- routes/basket.js
+++ routes/basket.js
@@ -1,14 +1,27 @@
 const basketRoute = require('express').Router();
-const { Basket, Drug } = require('../db/models');
+const { Basket, Drug, Order } = require('../db/models');
+
+function findTotal(arr) {
+  const price = arr.filter((basket) => basket['Drug.havePromo'] === false);
+  let total = 0;
+  // eslint-disable-next-line no-return-assign
+  return total = price.map((el) => el['Drug.price']).reduce((sum, el) => sum + el, 0);
+}
+
+function findDiscount(array) {
+  const discount = array.filter((basket) => basket['Drug.havePromo'] === true);
+  let totalDiscount = 0;
+  // eslint-disable-next-line no-return-assign
+  return totalDiscount = discount.map((el) => el['Drug.price']).reduce((sum, el) => sum + el, 0);
+}
 
 basketRoute.post('/addToBasket', async (req, res) => {
-  const kor = await Basket.create({
+  await Basket.create({
     drugId: req.body.id,
     userId: req.session.user.id,
     createdAt: new Date(),
     updatedAt: new Date(),
   });
-  console.log(kor);
   res.end();
 });
 
@@ -23,7 +36,71 @@ basketRoute.get('/basket', async (req, res) => {
       model: Drug,
     },
   });
-  res.render('basket', { allBaskets });
+
+  const total = findTotal(allBaskets);
+  const totalDiscount = findDiscount(allBaskets);
+
+  res.render('basket', { allBaskets, total, totalDiscount });
+});
+
+basketRoute.post('/addToOrder', (req, res) => {
+  const { total } = req.body;
+  res.render('order', {
+    total,
+    layout: false,
+  });
+});
+
+basketRoute.get('/order', async (req, res) => {
+  const allBaskets = await Basket.findAll({
+    raw: true,
+    where: [
+      { userId: req.session.user.id },
+      { orderId: null },
+    ],
+    include: {
+      model: Drug,
+    },
+  });
+  const total = findTotal(allBaskets);
+  res.render('order', { total });
+});
+
+basketRoute.post('/order/status', async (req, res) => {
+  const { adress, tel } = req.body;
+  // console.log(adress, tel);
+  const allBaskets = await Basket.findAll({
+    raw: true,
+    where: [
+      { userId: req.session.user.id },
+      { orderId: null },
+    ],
+    include: {
+      model: Drug,
+    },
+  });
+  const total = findTotal(allBaskets);
+  // console.log(adress, tel, total);
+  const order = await Order.create({
+    adress,
+    tel,
+    total,
+    userId: req.session.user.id,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  await Basket.update(
+    {
+      orderId: order.id,
+    },
+    {
+      where: [
+        { orderId: null },
+        { userId: req.session.user.id },
+      ],
+    },
+  );
+  res.render('success');
 });
 
 module.exports = basketRoute;
