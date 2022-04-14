f88326601b26b62dfe231d4b750ad91647ce796d
Svetlana <lirica1109@gmail.com>
2022-04-14 11:03:50 +0300

    сортировка

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const indexRoute = require('express').Router();
 // const checkAuth = require('../middleware/checkAuth');
-const  { isAuthorized } = require('../middleware/isAuthorized');
 const { Drug } = require('../db/models');
 
 indexRoute.get('/', async (req, res) => {
-  const drugs = await Drug.findAll({ raw: true });
-  // console.log(drugs);
+  const drugs = await Drug.findAll({
+    raw: true,
+    order: [['price', 'DESC']],
+  });
   res.render('index', { drugs });
 });
 
