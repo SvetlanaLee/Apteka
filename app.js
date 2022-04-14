32c86d82ca4090fceb392c9e8c771e8bef45ca4c
OlegBovkush <obovkush@gmail.com>
2022-04-14 17:11:08 +1000

    Работает

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 require('dotenv').config();
 require('./config/passport-google');
-
-const dbConnectionCheck = require("./db/dbConnectionCheck");
-
-require('dotenv').config();
-require('./config/passport-google');
-
 const express = require('express');
 const passport = require('passport');
 const path = require('path');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const cookieParser = require("cookie-parser"); // библиотека необходимая для чтения дынных с куки
-const expressSession = require("express-session");
-const FileStore = require("session-file-store")(expressSession);
-const indexRoute = require("./routes/index");
-const registrRoute = require("./routes/users/registr");
-
-// const sequelize = require('sequelize');
 let https;
 try {
   https = require('https');
@@ -27,8 +14,6 @@ try {
 }
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-
-
 const cookieParser = require('cookie-parser'); // библиотека необходимая для чтения дынных с куки
 const expressSession = require('express-session');
 const FileStore = require('session-file-store')(expressSession);
@@ -38,17 +23,10 @@ const indexRoute = require('./routes/index');
 const registrRoute = require('./routes/users/registr');
 const profileRoute = require('./routes/users/profile');
 
-
 // для сохранности сессий в наших данных
 const sessionConfig = {
   name: 'cookie',
   store: new FileStore(), // добавить после установки session-file-store
-
-
-  secret: "keyboard cat",
-  secret: 'secret',
-  
-=======
   secret: 'keyboard cat',
   cookie: {
     maxAge: 24 * 60 * 60 * 1000, // устанавливаем сколько живет кука
@@ -64,32 +42,19 @@ app.use(expressSession(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, "public/")));
-=======
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
-
-app.use("/", indexRoute);
-app.use("/", registrRoute);
 
 app.use(isAuthorized);
 app.use('/', indexRoute);
 app.use('/', registrRoute);
 app.use('/profile', profileRoute);
 
-
-
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 dbConnectionCheck();
-
 // https
 //   .createServer(
 //     // Provide the private and public key to the server by reading each
@@ -102,7 +67,6 @@ dbConnectionCheck();
 //   ).listen(PORT, (req, res) => {
 //     console.log('Сервер стартовал по протоколу HTTPS, порт:', PORT);
 //   });
-
 app.listen(3000, (req, res) => {
   console.log('Сервер стартовал по протоколу HTTP, порт:', 3000);
 });
